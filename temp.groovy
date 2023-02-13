f0c9e2124425afbc8511adeeb217d37eca47980c
akpandeyrx <124550095+akpandeyrx@users.noreply.github.com>
2023-02-13 13:20:07 +0530

    Update temp.groovy

--- temp.groovy
+++ temp.groovy
@@ -1,36 +1,6 @@
-pipelineJob("Testing") {        
-    logRotator {
-        daysToKeep(20)
-        numToKeep(20)
-    }
-    parameters {
-        stringParam {
-            name('REPO_URL')
-            defaultValue('')
-            description('')
-            trim(false)
-        }
-    }
-    definition {
-        cpsFlowDefinition {
-            sandbox(true)
-            script('''
-           pipeline{
-    agent any
-    
-     stages {
-        stage ("Build 1")  {
-        steps {
-        build job: "$GIT_RELEASE_PATH", parameters: [
-                string(name: 'REPO_URL', value: "${REPO_URL}"),
-                
-         sh   'printenv'  
-         sh "echo $REPO_URL"
-        }
-        }
+folder('folder-a') {
+    description('Folder containing all jobs for folder-a')
 }
-}
-            ''' )
-}
-    }
+job('folder-a/job-a') {
+    // Job config goes here
 }
