9a8d8e77655b542f6cc96afe8a3550fa0f7ae4f2
akpandeyrx <124550095+akpandeyrx@users.noreply.github.com>
2023-02-13 14:36:11 +0530

    Update temp.groovy

--- temp.groovy
+++ temp.groovy
@@ -1,6 +1,8 @@
+  jobDsl scriptText: '''import jenkins.model.Jenkins
 folder("${FolderName}") {
     description('Folder containing all jobs for folder-a')
 }
 job('${FolderName}/job-a') {
     // Job config goes here
 }
+'''
